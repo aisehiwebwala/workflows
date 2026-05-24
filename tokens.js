0d1ef74ae95219bf837a4bdce55a8fad74b833d7
aisehiwebwala <backw777@gmail.com>
2026-05-24 15:31:57 +0530

    Update token lists to include additional songs and artists

--- tokens.js
+++ tokens.js
@@ -22,9 +22,9 @@ const album_lists = ['51wWO8XAY-4_', 'jg5reqzpT7k_',
     'erQGXQItIFI_', 'QpcbT2oOB74_',
     'vk2wVV8fSg0_', 'Wky5mcRgfbc_',
     'sUaYu26J-UA_', 'zcVCNzg6Uvw_',
-    "evTOS8ealLo_","aHri6Xb32P0_","MAx1NsLoGJ4_", "WraOviBrkpo_", "EPJXYk,wkdA_", "men3PmZB4w0_", "iu3aaqmoeIk_", "KUHo4K,LD7A_", "GcR7iFIi5gE_", "8Ps4qqBA6,Y_", "MJVyyWfhWe4_", "RvGqKSIL2ZY_", "kw5AWtM1BZk_", "VCjKuSJcwxs_", "ft4MGKjYem0_", "fzGxJ2eyORk_", "k,TIgv1ORbc_", "HR757XLeV10_", "kLG-OKbVmvM_", "C3Br8V0qKrc_", "MpkyruMLllM_", "G1WcU,XEkfI_", "ALuu2RMhPDI_", "bGnqYFTPFRU_", "jg5reqzpT7k_", "PJIEIawr5j0_", "HImhTnw6KWw_", "l2uT56HmBl8_", "wSM2AOubajk_"]
-const song_lists = ["NwICVTt9BHI", "O1A8ACFWfXs", "QA09Uz5gUUk"]
-const artist_lists = ["LlRWpHzy3Hk_","OaFg9HPZgq8_","YNIh2poJKsY_"]
+    "0nM6LTOje2o_","q5Ns9BM,JsY_","GemmP7PcG2s_","Oov9QjDBm3Q_", "evTOS8ealLo_","aHri6Xb32P0_","MAx1NsLoGJ4_", "WraOviBrkpo_", "EPJXYk,wkdA_", "men3PmZB4w0_", "iu3aaqmoeIk_", "KUHo4K,LD7A_", "GcR7iFIi5gE_", "8Ps4qqBA6,Y_", "MJVyyWfhWe4_", "RvGqKSIL2ZY_", "kw5AWtM1BZk_", "VCjKuSJcwxs_", "ft4MGKjYem0_", "fzGxJ2eyORk_", "k,TIgv1ORbc_", "HR757XLeV10_", "kLG-OKbVmvM_", "C3Br8V0qKrc_", "MpkyruMLllM_", "G1WcU,XEkfI_", "ALuu2RMhPDI_", "bGnqYFTPFRU_", "jg5reqzpT7k_", "PJIEIawr5j0_", "HImhTnw6KWw_", "l2uT56HmBl8_", "wSM2AOubajk_"]
+const song_lists = ["NwICVTt9BHI", "O1A8ACFWfXs", "QA09Uz5gUUk","Fj9GfAxDWUY", "OSxGRQRScGM", "I1sPdgJoZFE", "QyEEdT8IRno", "BSY8VR9ZAgM", "HhsmcD12Wnc", "GQwEfT55dEY", "GwxTeyd0VXE", "LzIdSSZHUnI", "NT0qUzx5AAI", "FhFaYi10BgQ", "SCY0Bz0IcF0", "KV4ZCQIFb0Y", "Mg0zcxdSYXg", "MgAgYUJgGns", "Ph4gXTt1fmE", "Jy9TVEBmW3A", "HzAzVDoEcEU", "FhFTVToCfmw", "AV8FRy1VZXU", "JgklWSZ6f0E", "QT1dXSBqYX4", "NF4uBDdHcn8", "IzdcWSNjUnY"]
+const artist_lists = ["LlRWpHzy3Hk_","OaFg9HPZgq8_","YNIh2poJKsY_","-f6Su9-0agk_","h,lBxNUy,aE_","hLOr3AxVxXk_","deJJWFd1ItE_","bWIDsVrU6DE_"]
 
 module.exports = {
     album_lists,
